f8338cbcc3c6cab73729e6d0be8dde975eff42fe
Wang Zhuochun <stone1551@gmail.com>
2012-03-11 12:13:02 +0800

    add accordion to Announcement

--- data/js/tabs.js
+++ data/js/tabs.js
@@ -1,8 +1,9 @@
 // Author: Wang Zhuochun
-// Last Edit: 01/Mar/2012 03:04 AM
+// Last Edit: 11/Mar/2012 11:52 AM
 
 $(document).ready(function() {
-  $(".hide-tab").hide();
+  $(".hide-tab").hide("fast"); // hide tabs
+  $(".ann-content").slideUp("fast"); // hide ann-content
 
   $("#nav li").click(function() {
     if ( !$(this).hasClass("tab-selected") ) {
@@ -16,6 +17,23 @@ $(document).ready(function() {
       $("#" + oldTab + "-tab").toggleClass("hide-tab");
       $("#" + newTab + "-tab").show();
       $("#" + oldTab + "-tab").hide();
+    }
+  });
+
+  $(".ann-item").click(function() {
+    if ( $(this).hasClass("ann-selected") ) {
+      $(this).removeClass("ann-selected");
+      $(".ann-content", this).slideToggle("fast");
+    } else {
+      var oldAnnItem = $(".ann-selected");
+      if (oldAnnItem) {
+          $(".ann-content", oldAnnItem).slideToggle("fast");
+          oldAnnItem.removeClass("ann-selected");
+      }
+
+      // append class to newly selected
+      $(this).addClass("ann-selected");
+      $(".ann-content", this).slideToggle("fast");
     }
   });
 });
